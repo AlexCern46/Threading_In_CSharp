78c4cdc194b117fd9d8b1710da67ce10407dcd3f
sasha <sasha@DESKTOP-VT96VM3>
2023-09-27 09:36:25 +0300

    Добавьте файлы проекта.

---
+++ SP_lab2/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace SP_lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBoxNa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNThreads = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxK = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDeltaThreads = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxDeltaK = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxNa
+            // 
+            this.textBoxNa.Location = new System.Drawing.Point(15, 25);
+            this.textBoxNa.Name = "textBoxNa";
+            this.textBoxNa.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNa.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите кол-во элементов массива N_a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите кол-во потоков N_threads:";
+            // 
+            // textBoxNThreads
+            // 
+            this.textBoxNThreads.Location = new System.Drawing.Point(15, 73);
+            this.textBoxNThreads.Name = "textBoxNThreads";
+            this.textBoxNThreads.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNThreads.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите параметр сложности К:";
+            // 
+            // textBoxK
+            // 
+            this.textBoxK.Location = new System.Drawing.Point(15, 124);
+            this.textBoxK.Name = "textBoxK";
+            this.textBoxK.Size = new System.Drawing.Size(100, 20);
+            this.textBoxK.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(282, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Введите шаг изменения кол-ва потоков Delta_threads:";
+            // 
+            // textBoxDeltaThreads
+            // 
+            this.textBoxDeltaThreads.Location = new System.Drawing.Point(15, 177);
+            this.textBoxDeltaThreads.Name = "textBoxDeltaThreads";
+            this.textBoxDeltaThreads.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDeltaThreads.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(290, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Введите шаг изменения параметра сложности Delta_K:";
+            // 
+            // textBoxDeltaK
+            // 
+            this.textBoxDeltaK.Location = new System.Drawing.Point(15, 230);
+            this.textBoxDeltaK.Name = "textBoxDeltaK";
+            this.textBoxDeltaK.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDeltaK.TabIndex = 8;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(15, 371);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(290, 67);
+            this.buttonStart.TabIndex = 10;
+            this.buttonStart.Text = "button1";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 274);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 287);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "label7";
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(331, 6);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(457, 218);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(331, 230);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(457, 218);
+            this.chart2.TabIndex = 14;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxDeltaK);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDeltaThreads);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxNThreads);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNThreads;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxK;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDeltaThreads;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxDeltaK;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ SP_lab2/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Management;
+
+namespace SP_lab2
+{
+    public partial class Form1 : Form
+    {
+        private double[] a;
+        private double[] b;
+        private int N_a;
+        private int N_threads;
+        private int K;
+        private int Delta_threads;
+        private int Delta_K;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            chart1.Series.Clear();
+            chart1.Series.Add("Single");
+            chart1.Series.Add("Multiple");
+            chart1.Series["Single"].ChartType = SeriesChartType.Line;
+            chart1.Series["Multiple"].ChartType = SeriesChartType.Line;
+            chart1.Series["Single"].Color = Color.Red;
+            chart1.Series["Multiple"].Color = Color.Blue;
+            chart1.Series["Single"].BorderWidth = 3;
+            chart1.Series["Multiple"].BorderWidth = 3;
+            chart1.ChartAreas[0].AxisX.Title = "Количество потоков";
+            chart1.ChartAreas[0].AxisY.Title = "Время, мс";
+
+            chart2.Series.Clear();
+            chart2.Series.Add("Single");
+            chart2.Series.Add("Multiple");
+            chart2.Series["Single"].ChartType = SeriesChartType.Line;
+            chart2.Series["Multiple"].ChartType = SeriesChartType.Line;
+            chart2.Series["Single"].Color = Color.Red;
+            chart2.Series["Multiple"].Color = Color.Blue;
+            chart2.Series["Single"].BorderWidth = 3;
+            chart2.Series["Multiple"].BorderWidth = 3;
+            chart2.ChartAreas[0].AxisX.Title = "Сложность";
+            chart2.ChartAreas[0].AxisY.Title = "Время, мс";
+
+
+        }
+
+        private void InitializeArrays()
+        {
+            Random random = new Random();
+            a = new double[N_a];
+            b = new double[N_a];
+
+            for (int i = 0; i < N_a; i++)
+            {
+                a[i] = random.NextDouble();
+            }
+        }
+
+        private void ProcessArray(int start, int end, int K)
+        {
+            for (int i = start; i < end; i++)
+            {
+                for (int j = 0; j < K; j++)
+                {
+                    b[i] += Math.Pow(a[i], 1.789);
+                }
+            }
+        }
+
+        private void SingleThreadedProcessing()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            for (int thr = 1; thr <= N_threads; thr += Delta_threads)
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+
+                ProcessArray(0, a.Length, K);
+
+                stopwatch.Stop();
+                chart1.Series["Single"].Points.AddXY(thr, stopwatch.ElapsedMilliseconds);
+            }
+
+            for (int k = 0; k < K; k += Delta_K)
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+
+                ProcessArray(0, a.Length, k);
+
+                stopwatch.Stop();
+                chart2.Series["Single"].Points.AddXY(k, stopwatch.ElapsedMilliseconds);
+            }
+
+        }
+
+        private void MultiThreadedProcessing()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            
+            for (int thr = 1; thr <= N_threads; thr += Delta_threads)
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+
+                Thread[] threads = new Thread[thr];
+                int delta = N_a / thr;
+                for (int i = 0; i < thr; i++)
+                {
+                    int start = i * delta;
+                    int end = (i + 1) * delta;
+                    if (i == thr - 1)
+                    {
+                        end = N_a;
+                    }
+                    threads[i] = new Thread(() => ProcessArray(start, end, K));
+                    threads[i].Start();
+                }
+                for (int i = 0; i < thr; i++)
+                {
+                    threads[i].Join();
+                }
+
+                stopwatch.Stop();
+                chart1.Series["Multiple"].Points.AddXY(thr, stopwatch.ElapsedMilliseconds);
+            }
+
+            for (int k = 0; k < K; k += Delta_K)
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+
+                Thread[] threads = new Thread[N_threads];
+                int delta = N_a / N_threads;
+                for (int i = 0; i < N_threads; i++)
+                {
+                    int start = i * delta;
+                    int end = (i + 1) * delta;
+                    if (i == N_threads - 1)
+                    {
+                        end = N_a;
+                    }
+                    threads[i] = new Thread(() => ProcessArray(start, end, k));
+                    threads[i].Start();
+                }
+                for (int i = 0; i < N_threads; i++)
+                {
+                    threads[i].Join();
+                }
+
+                stopwatch.Stop();
+                chart2.Series["Multiple"].Points.AddXY(k, stopwatch.ElapsedMilliseconds);
+            }
+        }
+
+        private void GetProcessorParameters()
+        {
+            
+            string clockSpeed = "";
+            string numberOfCores = "";
+            
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                clockSpeed = obj["CurrentClockSpeed"].ToString();
+                numberOfCores = obj["NumberOfCores"].ToString();
+            }
+            
+            label6.Text = "Тактовая частота: " + clockSpeed + " МГц";
+            label7.Text = "Количество ядер: " + numberOfCores;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Single"].Points.Clear();
+            chart1.Series["Multiple"].Points.Clear();
+            chart2.Series["Single"].Points.Clear();
+            chart2.Series["Multiple"].Points.Clear();
+
+            N_a = int.Parse(textBoxNa.Text);
+            N_threads = int.Parse(textBoxNThreads.Text);
+            K = int.Parse(textBoxK.Text);
+            Delta_threads = int.Parse(textBoxDeltaThreads.Text);
+            Delta_K = int.Parse(textBoxDeltaK.Text);
+            
+            InitializeArrays();
+            SingleThreadedProcessing();
+            MultiThreadedProcessing();
+            GetProcessorParameters();
+        }
+    }
+}
